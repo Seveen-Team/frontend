22a19f829eb453998ffc9ae8cf18c47f7a247c34
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-12-18 12:08:40 -0500

    [ADD] Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import LogoPM from '../../assets/logoPN.png';
+import LogoPM from '../../assets/logoPM.png';
+import Photo from '../../assets/hero.jpg';
+import { WrapperHeader, ImgLogo, WrapperUser, ImgPhoto, Name } from './styles';
 
-const Header = () => {
-  return <img src={LogoPM} />;
-};
+class Header extends React.Component {
+  render() {
+    return (
+      <WrapperHeader>
+        <ImgLogo src={LogoPM} />
+        <WrapperUser>
+          <ImgPhoto src={Photo} />
+          <Name>Pepito perez</Name>
+        </WrapperUser>
+      </WrapperHeader>
+    );
+  }
+}
 
 export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const WrapperHeader = styled.div`
+  width: 100vw;
+  background-color: #182c3f;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ImgLogo = styled.img`
+  widht: 210px;
+  height: 70px;
+`;
+
+const WrapperUser = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 15px;
+`;
+
+const ImgPhoto = styled.img`
+  height: 60px;
+  width: 60px;
+  border-radius: 50px;
+  object-fit: cover;
+`;
+
+const Name = styled.p`
+  color: white;
+  font-size: 24px;
+  font-family: sans-serif;
+  margin-left: 10px;
+`;
+
+export { WrapperHeader, ImgLogo, WrapperUser, ImgPhoto, Name };
