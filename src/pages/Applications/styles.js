4f5118f1a70be0d12d6fc7ee6884057063b23ba8
hadiazb <hadiazb@unal.edu.co>
2020-12-19 11:49:01 -0500

    [ADD] list of card

--- src/pages/Applications/styles.js
+++ src/pages/Applications/styles.js
@@ -4,3 +4,23 @@ export const Title = styled.h1`
   font-size: 32px;
   margin: 0 0 20px 0;
 `;
+
+export const ApplicationsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const ListContainer = styled.div`
+  width: 250px;
+  height: 500px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  border-radius: 2px;
+`;
+
+export const Subtitle = styled.h2`
+  width: 100%;
+  padding: 10px 20px;
+  background-color: #182c3f;
+  color: white;
+`;
