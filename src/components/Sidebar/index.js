5f883c8590c77d5f0d6b2cb809a9eaed1589a0f2
hadiazb <hadiazb@unal.edu.co>
2020-12-18 23:54:55 -0500

    [UPLOAD] conficts

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,7 +8,6 @@ const Sidebar = ({ open, action }) => {
   }
 
   return (
-<<<<<<< HEAD
     <WrapperSidebar>
       <Anchor to="/vacancies">
         <Text>Vacantes</Text>
@@ -16,20 +15,6 @@ const Sidebar = ({ open, action }) => {
       <Anchor to="/">
         <Text>Mis aplicaciones</Text>
       </Anchor>
-=======
-    <WrapperSidebar state={open}>
-      <CloseButton type='button' onClick={handleClose}>
-        X
-      </CloseButton>
-      <ListContent>
-        <Anchor to="/">
-          <Text>Vacantes</Text>
-        </Anchor>
-        <Anchor to="/test">
-          <Text>Mis aplicaciones</Text>
-        </Anchor>
-      </ListContent>
->>>>>>> 4b6721d58470b38a6f58a2b54f7c507b6efe8b45
     </WrapperSidebar>
   );
 }
--- src/routes/App.js
+++ src/routes/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import TrelloCard from '../components/TrelloCard';
-<<<<<<< HEAD
 import Vacancies from '../pages/Vacances';
-=======
-import Login from '../containers/Login';
->>>>>>> 4b6721d58470b38a6f58a2b54f7c507b6efe8b45
 
 const App = () => {
   return (
@@ -14,11 +10,7 @@ const App = () => {
       <Layout>
         <Switch>
           <Route exact path='/' component={TrelloCard} />
-<<<<<<< HEAD
           <Route exact path='/vacancies' component={Vacancies} />
-=======
-          <Route exact path='/login' component={Login} />
->>>>>>> 4b6721d58470b38a6f58a2b54f7c507b6efe8b45
         </Switch>
       </Layout>
     </BrowserRouter>
