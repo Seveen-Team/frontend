ee5faf0f13e157d5fbcbb545b40654ae2a4888c6
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-12-19 17:20:23 -0500

    [ADD] DashboardCard component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import GlobalStyles from './styles/GloblalStyles';
-import App from './routes/App';
+// import App from './routes/App';
 import Store from './store/index';
+import DashboardCard from './components/DashboardCard';
 
 ReactDOM.render(
   <Provider store={Store}>
     <GlobalStyles />
-    <App />
+    <DashboardCard />
   </Provider>,
   document.getElementById('app')
 );
