7db351d008a25184e3074a06ee949dddf34ad4ad
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-12-19 00:15:44 -0500

    [ADD] EmpresaModal

--- src/components/MasInfo/index.js
+++ src/components/MasInfo/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import InfoDetallada from '../InfoDetallada';
 import Chips from '../Chips';
 import styled from 'styled-components';
 
 const WrapperMasInfo = styled.div`
-  width: 100%;
+  width: 50%;
+  heigth: 50%;
+  box-shadow: 3px 3px 6px gray;
+  padding: 20px;
 `;
 
 const WrapperColumns = styled.div`
@@ -17,15 +19,28 @@ const WrapperLeft = styled.div`
 
 const WrapperRight = styled.div`
   width: 33%;
+  padding-left: 35px;
 `;
+
+const Title = styled.p`
+  width: 90%;
+  padding: 5px;
+  margin: 5px;
+  font-size: 24px;
+  font-family: Babes, sans-serif;
+`;
+
+const Details = styled.p``;
 
 const TitleCompany = styled.p`
   font-size: 34px;
 `;
 
-const TitleRequisitos = styled.p``;
+const List = styled.ul``;
 
-const DetailsRequisitos = styled.p``;
+const Item = styled.li`
+  margin-top: 15px;
+`;
 
 const MasInfo = () => {
   return (
@@ -34,11 +49,28 @@ const MasInfo = () => {
       <WrapperColumns>
         <WrapperLeft>
           <Chips />
-          <InfoDetallada />
+          <Title>Empresa</Title>
+          <Details>
+            Mono es una startup colombiana. Estamos creando una banca digital
+            para empresas en Colombia, somos un equipo con gran experiencia en
+            fintech.
+          </Details>
+          <Title>Dirección Web</Title>
+          <Details>Mono es una startup colombiana.</Details>
+          <Title>Responsabilidades</Title>
+          <Details>
+            Mono es una startup colombiana. Estamos creando una banca digital
+            para empresas en Colombia, somos un equipo con gran experiencia en
+            fintech.
+          </Details>
         </WrapperLeft>
         <WrapperRight>
-          <TitleRequisitos>Requisitos Técnicos</TitleRequisitos>
-          <DetailsRequisitos>JIjdiededj</DetailsRequisitos>
+          <Title>Requisitos Técnicos</Title>
+          <List>
+            <Item>Mono es una startup colombiana.</Item>
+            <Item>Mono es una startup colombiana.</Item>
+            <Item>Mono es una startup colombiana.</Item>
+          </List>
         </WrapperRight>
       </WrapperColumns>
     </WrapperMasInfo>
