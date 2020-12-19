e76dc146ad388dcaab2d45bd06abf08ce11736f3
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-12-19 17:23:14 -0500

    [UPLOAD] DashboardCard

--- src/components/DashboardCard/index.js
+++ src/components/DashboardCard/index.js
@@ -1,50 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const WrapperCard = styled.div`
-  color: white;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  text-align: center;
-  background: rgb(24, 44, 63);
-  background: linear-gradient(
-    180deg,
-    rgba(24, 44, 63, 1) 0%,
-    rgba(152, 202, 62, 1) 56%
-  );
-`;
-
-const WrapperImg = styled.div``;
-
-const TitleStats = styled.h1`
-  font-size: 40px;
-
-  margin: 20px;
-  color: white;
-`;
-
-const ImgPhoto = styled.img`
-  height: 150px;
-  width: 150px;
-  border-radius: 100px;
-  object-fit: cover;
-  margin: 20px;
-`;
-
-const Text = styled.p`
-  margin: 20px;
-  color: white;
-  font-size: 24px;
-  font-family: Babes, sans-serif;
-`;
-
-const Details = styled.p`
-  margin: 20px;
-  color: white;
-  font-size: 16px;
-  font-family: Babes, sans-serif;
-`;
+import {
+  Details,
+  Text,
+  ImgPhoto,
+  TitleStats,
+  WrapperImg,
+  WrapperCard,
+} from './styles';
 const DashboardCard = () => {
   const PhotoProfile =
     'https://firebasestorage.googleapis.com/v0/b/platzi-master-21d21.appspot.com/o/Assets%2Fhero.jpg?alt=media&token=e4b8cefe-ca98-42d5-aba7-1521e715edf6';
---
+++ src/components/DashboardCard/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+const WrapperCard = styled.div`
+  color: white;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  background: rgb(24, 44, 63);
+  background: linear-gradient(
+    180deg,
+    rgba(24, 44, 63, 1) 0%,
+    rgba(152, 202, 62, 1) 56%
+  );
+`;
+
+const WrapperImg = styled.div``;
+
+const TitleStats = styled.h1`
+  font-size: 40px;
+
+  margin: 20px;
+  color: white;
+`;
+
+const ImgPhoto = styled.img`
+  height: 150px;
+  width: 150px;
+  border-radius: 100px;
+  object-fit: cover;
+  margin: 20px;
+`;
+
+const Text = styled.p`
+  margin: 20px;
+  color: white;
+  font-size: 24px;
+  font-family: Babes, sans-serif;
+`;
+
+const Details = styled.p`
+  margin: 20px;
+  color: white;
+  font-size: 16px;
+  font-family: Babes, sans-serif;
+`;
+
+export { Details, Text, ImgPhoto, TitleStats, WrapperImg, WrapperCard };
