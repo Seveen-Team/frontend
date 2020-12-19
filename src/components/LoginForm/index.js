d8e9b31a485f39c4ef9bc7fd63d4e76bfd252002
hadiazb <hadiazb@unal.edu.co>
2020-12-19 09:49:06 -0500

    [ADD] style login

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -3,7 +3,6 @@ import { ThemeProvider } from 'styled-components';
 import { useDispatch } from 'react-redux';
 import Variables from '../../styles/vars';
 import { Form, Image, Input, Button, Anchor, Container } from './styles';
-import logo from '../../assets/logoPM.png';
 import { loginUser } from '../../actions/loginAction';
 
 const LoginForm = () => {
@@ -23,11 +22,12 @@ const LoginForm = () => {
     event.preventDefault();
     dispatch(loginUser(form, '/'));
   }
+  const Logo = "https://firebasestorage.googleapis.com/v0/b/platzi-master-21d21.appspot.com/o/Assets%2FlogoPM.png?alt=media&token=96438203-1626-42fb-aa18-f20ffc2abc3a";
 
   return (
     <ThemeProvider theme={Variables}>
       <Form onSubmit={handleLogin} autoComplete='off'>
-        <Image src={logo} />
+        <Image src={Logo} />
         <Container>
           <Input
             placeholder='Usuario'
