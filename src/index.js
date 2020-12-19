a078dc25714c564cbd638f7bb29335e52b4d1fcd
hadiazb <hadiazb@unal.edu.co>
2020-12-18 22:18:22 -0500

    [ADD] redux and global state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import GlobalStyles from './styles/GloblalStyles';
 import App from './routes/App';
+import Store from './store/index';
 
 ReactDOM.render(
-  <>
+  <Provider store={Store}>
     <GlobalStyles />
     <App />
-  </>, 
-  document.getElementById('app'));
+  </Provider>,
+  document.getElementById('app')
+);
